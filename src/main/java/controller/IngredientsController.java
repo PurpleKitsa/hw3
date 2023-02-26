be624a1f2eca65858e64dda193fae8d4dc86d6d1
PurpleKitsa <kapitandiman2@gmail.com>
2023-02-27 01:10:19 +0300

    Создание API. Postman

--- src/main/java/controller/IngredientsController.java
+++ src/main/java/controller/IngredientsController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import model.Ingredient;
-import model.Recipe;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.IngredientService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
@@ -15,12 +17,44 @@ public class IngredientsController {
     }
 
     @PostMapping
-    public void addIngredient(@RequestBody Ingredient ingredient) {
-        ingredientService.addIngredient(ingredient);
+    public ResponseEntity<Integer> addIngredient(@RequestBody Ingredient ingredient) {
+        int id = ingredientService.addIngredient(ingredient);
+        return ResponseEntity.ok(id);
     }
 
     @GetMapping("/{id}")
-    public Ingredient getIngredient(@PathVariable int id) {
-        return ingredientService.showIngredient(id);
+    public ResponseEntity getIngredientId(@PathVariable int id) {
+        Ingredient ingredient = ingredientService.showIngredient(id);
+        if (ingredient == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ingredient);
+    }
+
+    @PutMapping
+    public ResponseEntity getIngredient() {
+        Map<Integer, Ingredient> mapIngredient = ingredientService.listIngredient();
+
+        return ResponseEntity.ok(mapIngredient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingredient> editIngredient(@PathVariable int id, @RequestBody Ingredient ingredient) {
+        Ingredient ingredient1 = ingredientService.editIngredient(id, ingredient);
+        if (ingredient1 == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngredient(@PathVariable int id) {
+        if (ingredientService.deleteIngredient(id)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+
     }
 }
+
+
--- src/main/java/controller/RecipeController.java
+++ src/main/java/controller/RecipeController.java
@@ -4,6 +4,9 @@ import model.Recipe;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.RecipeService;
+
+import java.util.Map;
+
 @RestController
 @RequestMapping("/recipe")
 public class RecipeController {
@@ -18,9 +21,39 @@ public class RecipeController {
         recipeService.addRecipe(recipe);
     }
 
+    @GetMapping
+    public Recipe getRecipe() {
+        Map<Integer, Recipe> mapRecipe = recipeService.listRecipe();
+
+        return ResponseEntity.ok(mapRecipe);
+    }
+
     @GetMapping("/{id}")
-    public Recipe getRecipe(@PathVariable int id) {
-        return recipeService.showRecipe(id);
+    public ResponseEntity getRecipeId(@PathVariable int id) {
+        Recipe recipe = recipeService.showRecipe(id);
+        if (recipe == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(recipe);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Recipe> editRecipe(@PathVariable int id, @RequestBody Recipe recipe) {
+        Recipe recipe1 = recipeService.editRecipe(id, recipe);
+        if (recipe1 == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(recipe);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRecipe(@PathVariable int id) {
+        if (recipeService.deleteRecipe(id)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+
     }
 }
 
